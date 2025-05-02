003a906b9b815c6ef15ce1f933df3e585f06a312
Mayck Xavier <mayckxavier@gmail.com>
2025-05-02 00:59:52 -0300

    Add error handling and logging for database connection in base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,13 +1,25 @@
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.core.config import settings
 
-# Create the SQLAlchemy engine
-engine = create_engine(
-    settings.DATABASE_URL, connect_args={"check_same_thread": False}
-)
+from app.db.base_class import Base
+from app.models.part import Part
+
+try:
+    # Create the SQLAlchemy engine
+    engine = create_engine(
+        settings.DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+    with engine.connect() as conn:
+        logging.info("Conexão com o banco de dados estabelecida com sucesso")
+except SQLAlchemyError as e:
+    logging.error(f"Erro de conexão com o banco de dados: {str(e)}")
+
 # The connect_args parameter is needed only for SQLite. For other databases like PostgreSQL, remove it.
 
 # Create a SessionLocal class
