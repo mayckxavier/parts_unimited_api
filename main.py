ba161e3d995922f21353b9a4e43c41e03677aa5f
Mayck Xavier <mayckxavier@gmail.com>
2025-05-02 01:12:09 -0300

    Integrate API router and custom exception handlers into FastAPI application

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.api import api_router
 from app.core.config import settings
+from app.core.exceptions import add_exception_handlers
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -18,6 +20,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(api_router, prefix=settings.API_V1_STR)
+add_exception_handlers(app)
 
 # Import and include routers here (will be implemented later)
 
